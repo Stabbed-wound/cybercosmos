4a3f2cb3bcf47979623bac9e0aec9e92ce8ae4c4
GavGav <90939901+Stabbed-wound@users.noreply.github.com>
2022-03-28 23:56:55 +0000

    firebase pt1.5

--- src/CharacterSheet.js
+++ src/CharacterSheet.js
@@ -1,12 +1,40 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable jsx-a11y/anchor-has-content */
 import React from "react";
-import Armour from "./Armour";
-import Stat from "./Stat";
-import Skill from "./Skill";
-import Weapon from "./Weapon";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBSAdJZCYy_RCvRdC0iDOEqgaLCTBQFVoY",
+  authDomain: "cybercosmos-46c91.firebaseapp.com",
+  projectId: "cybercosmos-46c91",
+  storageBucket: "cybercosmos-46c91.appspot.com",
+  messagingSenderId: "972184976227",
+  appId: "1:972184976227:web:d248034258b08eb730f156"
+};
+
+const app = initializeApp(firebaseConfig);
 
 export default function CharacterSheet() {
+	const Armour = () => {
+		return (
+			<div>Armour</div>
+		);
+	}
+	const Weapon = () => {
+		return (
+			<div>Weapon</div>
+		);
+	}
+	const Stat = () => {
+		return (
+			<div>Stat</div>
+		);
+	}
+	const Skill = () => {
+		return (
+			<div>Skill</div>
+		);
+	}
 	return (
 		<>
 			<div id="stats">
--- src/Stat.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Stat({ name, value }) {
-	return <div id="stat"></div>;
-}
