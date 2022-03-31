012bb124d146b38aea529265251a59acf3872d51
GavGav <90939901+Stabbed-wound@users.noreply.github.com>
2022-03-31 03:12:04 +0000

    made stats better

--- src/CharacterSheet.js
+++ src/CharacterSheet.js
@@ -4,36 +4,40 @@ import "./style.css";
 export default function CharacterSheet() {
 	return (
 		<div class="my-10 text-center mx-48 h-screen">
-			<div class="float-left h-full">
-				<Stat name={"INT"} value={7} />
-				<Stat name={"REF"} value={7} />
-				<Stat name={"DEX"} value={7} />
-				<Stat name={"TECH"} value={4} />
-				<Stat name={"COOL"} value={7} />
-				<Stat name={"WILL"} value={6} />
-				<Stat name={"LUCK"} value={6} />
-				<Stat name={"MOVE"} value={5} />
-				<Stat name={"BODY"} value={6} />
-				<Stat name={"EMP"} value={6} />
-			</div>
-			<div class="float-left"></div>
+			<div class="h-32 w-1/3"></div>
+			<Stats values={[7,7,7,4,7,6,5,6]}/>
 		</div>
 	);
 }
 
-function Stat({ name, value, outOf }) {
-	return (
-		<div class="bg-cyberblack border-cyberred rounded-lg border-[3px] text-cybergrey w-20 h-20 relative">
-			<div class="h-min w-min border-cyberred border-l-2 border-b-2 font-medium leading-none text-lg absolute top-0 right-0">
-				{name}
-			</div>
-			{outOf ? (
-				<div></div>
-			) : (
-				<div class="text-4xl mt-1 h-max w-max absolute bottom-0">
-					{value}
+
+
+function Stats({values}) {
+	function Stat({ name, value }) {
+		return (
+			<div class="flex flex-auto justify-center">
+				<div class="bg-cyberblack border-cyberred rounded-lg border-[3px] text-cybergrey w-20 h-20 relative">
+					<div class="h-min w-min border-cyberred border-l-2 border-b-2 font-medium leading-tight text-lg absolute top-0 right-0">
+						{name}
+					</div>
+					<div class="text-5xl absolute bottom-0 right-1/2">
+						{value}
+						</div>
 				</div>
-			)}
+			</div>
+		);
+	}
+	
+	return (
+		<div class="flex flex-wrap">
+			<Stat name={"INT"} value={values[0]} />
+			<Stat name={"REF"} value={values[1]} />
+			<Stat name={"DEX"} value={values[2]} />
+			<Stat name={"TECH"} value={values[3]} />
+			<Stat name={"COOL"} value={values[4]} />
+			<Stat name={"WILL"} value={values[5]} />
+			<Stat name={"MOVE"} value={values[6]} />
+			<Stat name={"BODY"} value={values[7]} />
 		</div>
-	);
+	)
 }
